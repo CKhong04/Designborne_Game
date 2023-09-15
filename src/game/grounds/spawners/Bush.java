7ff13ffd8357abb21fd188becf75efc7e443b7dd
Carissa Khong <ckho0027@student.monash.edu>
2023-09-15 11:04:53 +1000

    Added four new classes, Bush, Hut, Red Wolf and Forest Keeper. These classes represent new enemies or new spawning grounds for these enemies.

---
+++ src/game/grounds/spawners/Bush.java
@@ -0,0 +1,31 @@
+package game.grounds.spawners;
+
+import edu.monash.fit2099.engine.positions.Location;
+import game.actors.enemies.RedWolf;
+
+/**
+ * The Bush class extends the SpawningGround class and has a chance of spawning Red Wolves from it each turn.
+ * Created by:
+ * @author Carissa Khong
+ */
+public class Bush extends SpawningGround{
+
+    /**
+     * This is the constructor for the Bush class.
+     *
+     * Bushes have a 30% chance of spawning an enemy each turn and are displayed with the character 'm' on the map.
+     */
+    public Bush() {
+        super(30, 'm');
+    }
+
+    /**
+     * The tick method of the Bush class is called every turn. It will call the spawnEnemy() method which will be able
+     * to spawn a new Red Wolf enemy.
+     * @param location The location of the Bush.
+     */
+    @Override
+    public void tick(Location location) {
+        super.spawnEnemy(new RedWolf(), location);
+    }
+}
---
+++ src/game/grounds/spawners/Hut.java
@@ -0,0 +1,30 @@
+package game.grounds.spawners;
+
+import edu.monash.fit2099.engine.positions.Location;
+import game.actors.enemies.ForestKeeper;
+
+/**
+ *The Hut class extends from the abstract class SpawningGround and has the chance of spawning a Forest Keeper on each
+ * turn.
+ * Created by:
+ * @author Carissa Khong
+ */
+public class Hut extends SpawningGround {
+    /**
+     * Constructor.
+     * An instance of the Hut class may spawn an enemy with a chance of 15% each turn. The display symbol of a hut is 'h'.
+     */
+    public Hut() {
+        super(15, 'h');
+    }
+
+    /**
+     * The tick method calls the spawnEnemy() method from the SpawningGround class. When called each turn, it can spawn
+     * a new ForestKeeper, at the location of the Hut.
+     * @param location The location of the Hut.
+     */
+    @Override
+    public void tick(Location location) {
+        super.spawnEnemy(new ForestKeeper(),location);
+    }
+}
