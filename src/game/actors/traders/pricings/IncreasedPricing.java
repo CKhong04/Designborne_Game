bc9e957d1d0fe0d4362c4b995ebbfe8206a308cb
laurazhak <lzha0181@student.monash.edu>
2023-09-17 18:33:14 +1000

    fix calculations

--- src/game/actors/traders/pricings/IncreasedPricing.java
+++ src/game/actors/traders/pricings/IncreasedPricing.java
@@ -4,17 +4,17 @@ import java.util.Random;
 
 public class IncreasedPricing extends Pricing {
     private int chanceToIncrease;
-    private int percentage;
-    public IncreasedPricing(int chanceToIncrease, int percentage){
+    private int percentageIncrease;
+    public IncreasedPricing(int chanceToIncrease, int percentageIncrease){
         this.chanceToIncrease = chanceToIncrease;
-        this.percentage = percentage;
+        this.percentageIncrease = percentageIncrease;
     }
 
     @Override
     public int getPrice(int originalPrice){
         Random rand = new Random();
         if (rand.nextInt(100) <= this.chanceToIncrease) {
-            return originalPrice * (1+this.percentage);
+            return originalPrice * (1+(this.percentageIncrease/100));
         } else {
             return originalPrice;
         }
--- src/game/actors/traders/pricings/ReducedPricing.java
+++ src/game/actors/traders/pricings/ReducedPricing.java
@@ -4,17 +4,17 @@ import java.util.Random;
 
 public class ReducedPricing extends Pricing {
     private int chanceToIncrease;
-    private int percentage;
-    public ReducedPricing(int chanceToIncrease, int percentage){
+    private int percentageDecrease;
+    public ReducedPricing(int chanceToIncrease, int percentageDecrease){
         this.chanceToIncrease = chanceToIncrease;
-        this.percentage = percentage;
+        this.percentageDecrease = percentageDecrease;
     }
 
     @Override
     public int getPrice(int originalPrice){
         Random rand = new Random();
         if (rand.nextInt(100) <= this.chanceToIncrease) {
-            return originalPrice * (1-this.percentage);
+            return originalPrice * (1-(this.percentageDecrease/100));
         } else {
             return originalPrice;
         }
