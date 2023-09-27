d8141580b696330fa50f4167129f5972ab45effd
laurazhak <lzha0181@student.monash.edu>
2023-09-27 15:11:30 +1000

    write comments

--- src/game/weathers/AncientWoodEntity.java
+++ src/game/weathers/AncientWoodEntity.java
@@ -1,5 +1,10 @@
 package game.weathers;
-
+/**
+ * Interface representing an Ancient Wood Entity.
+ * Created by:
+ * @author Laura Zhakupova
+ * Modified by:
+ */
 public interface AncientWoodEntity {
     void sunnyUpdate();
     void rainyUpdate();
