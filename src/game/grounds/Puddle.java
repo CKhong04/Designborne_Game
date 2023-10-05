bc38225090de91bd6b5160023c02bafa38503a2b
Ishita <80767328+weavingandwandering@users.noreply.github.com>
2023-10-05 10:24:18 +0800

    -DrinkAction deleted -Puddle made into a Consumable Item

--- src/game/grounds/Puddle.java
+++ src/game/grounds/Puddle.java
@@ -2,10 +2,13 @@ package game.grounds;
 
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actors.attributes.ActorAttributeOperations;
+import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
 import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
-import game.actions.DrinkAction;
+import game.actions.ConsumeAction;
 import game.enums.Status;
+import game.items.itemproperties.Consumable;
 
 /**
  * A class that represents a puddle.
@@ -13,7 +16,7 @@ import game.enums.Status;
  * @author
  * Modified by: Ishita Gupta
  */
-public class Puddle extends Ground {
+public class Puddle extends Ground implements Consumable {
     public Puddle() {
         super('~');
     }
@@ -22,9 +25,26 @@ public class Puddle extends Ground {
         ActionList actions = super.allowableActions(actor, location, direction);
         Actor currActor = location.getActor();
         if (currActor != null && currActor.hasCapability(Status.DRINK_WATER)) {
-            actions.add(new DrinkAction());
+            actions.add(new ConsumeAction(this));
         }
         return actions;
+    }
+
+    @Override
+    public void consumeItem(Actor actor) {
+        int currentStamina = actor.getAttribute(BaseActorAttributes.STAMINA);
+        int maxStamina = actor.getAttributeMaximum(BaseActorAttributes.STAMINA);
+        int currentHP = actor.getAttribute(BaseActorAttributes.HEALTH);
+
+        if (currentStamina < maxStamina){
+            int recoverPercentage = maxStamina / 100;
+            actor.modifyAttribute(BaseActorAttributes.STAMINA, ActorAttributeOperations.INCREASE,recoverPercentage);
+        }
+        actor.modifyAttribute(BaseActorAttributes.HEALTH,ActorAttributeOperations.UPDATE,1+currentHP);
+    }
+
+    public String toString(){
+        return "water from Puddle";
     }
 }
 
--- src/game/items/itemproperties/Consumable.java
+++ src/game/items/itemproperties/Consumable.java
@@ -5,4 +5,7 @@ import edu.monash.fit2099.engine.actors.Actor;
 public interface Consumable {
 
     void consumeItem(Actor actor);
+
+    String toString();
+
 }
