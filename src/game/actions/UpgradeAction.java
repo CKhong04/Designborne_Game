d294e3c6cd16cf8bd58f82cb6ea3eece679dadb3
Ishita <80767328+weavingandwandering@users.noreply.github.com>
2023-10-12 11:29:29 +0800

    String for menuDescription added

--- src/game/actions/UpgradeAction.java
+++ src/game/actions/UpgradeAction.java
@@ -3,7 +3,6 @@ package game.actions;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
-import game.items.itemproperties.Consumable;
 import game.items.itemproperties.Upgradable;
 
 public class UpgradeAction extends Action {
