7a49635b44d940cf177a141dd67b8792d3865d9a
Ishita <80767328+weavingandwandering@users.noreply.github.com>
2023-10-28 21:08:23 +0800

    javadoc

--- src/game/actions/UpgradeAction.java
+++ src/game/actions/UpgradeAction.java
@@ -10,11 +10,28 @@ public class UpgradeAction extends Action {
     private final Upgradeable item;
     private final int upgradePrice;
 
+    /**
+     * The constructor of the UpgradeAction function
+     *
+     * @param item Determine which item is being upgraded
+     * @param upgradePrice Determine the price of the upgrade
+     *
+     *
+     */
     public UpgradeAction(Upgradeable item, int upgradePrice){
         this.item = item;
         this.upgradePrice = upgradePrice;
     }
 
+    /**
+     * The execute function takes in an actor and a map, and upgrades the item if the actor has enough runes.
+     *
+     * @param actor Determine the name of the actor performing
+     * @param map the map in which the player is
+     *
+     * @return A string
+
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         if (actor.getBalance() < this.upgradePrice){
@@ -25,6 +42,13 @@ public class UpgradeAction extends Action {
         }
     }
 
+    /**
+     * The menuDescription function returns a string that is the description of the upgradeAction.
+     *
+     * @param actor Determine the player's inventory
+     *
+     * @return A string that describes the action
+     */
     @Override
     public String menuDescription(Actor actor) {
         return "Upgrade " + this.item + " for " + upgradePrice + " runes";
