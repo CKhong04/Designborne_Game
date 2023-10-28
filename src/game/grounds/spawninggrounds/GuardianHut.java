9cefe4b50b272c7e7d675e1ff20245755beaa03d
Carissa Khong <ckho0027@student.monash.edu>
2023-10-28 23:37:07 +1100

    Added some docstrings which were missing for the new Bush and Hut class.

--- src/game/grounds/spawninggrounds/GuardianHut.java
+++ src/game/grounds/spawninggrounds/GuardianHut.java
@@ -3,7 +3,15 @@ package game.grounds.spawninggrounds;
 import edu.monash.fit2099.engine.positions.Location;
 import game.spawners.Spawners;
 
+/**
+ * GuardianHut is a class which extends from the SpawningGround class. It is able to spawn new Eldentree Guardians with
+ * a certain chance each turn.
+ * Created by:
+ * @author Carissa Khong
+ */
 public class GuardianHut extends SpawningGround{
+
+    //Private attribute
     private final static int CHANCE_TO_SPAWN = 20;
 
     /**
--- src/game/grounds/spawninggrounds/LivingBranchBush.java
+++ src/game/grounds/spawninggrounds/LivingBranchBush.java
@@ -3,7 +3,15 @@ package game.grounds.spawninggrounds;
 import edu.monash.fit2099.engine.positions.Location;
 import game.spawners.Spawners;
 
+/**
+ * LivingBranchBush is a class which extends from SpawningGround. It is able to spawn Living Branch enemies with a
+ * certain chance each turn.
+ * Created by:
+ * @author Carissa Khong
+ */
 public class LivingBranchBush extends SpawningGround{
+
+    //Private attribute
     private final static int CHANCE_TO_SPAWN = 90;
     /**
      * Constructor for the Living Branch bush.
